e572a884dc135953ac12c8b0e82b2ef03afd6437
luishecastilho <manocastilho07@gmail.com>
2023-05-15 23:15:59 -0300

    routes created

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use \App\Http\Controllers\{
+    AuthController,
+    UserController,
+    FeedController
+};
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::prefix('auth')->group(function () {
+        // login oauth...
+        Route::post('/', [AuthController::class, 'store'])->name('auth.store');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    });
+
+    Route::prefix('user')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('user.index');
+        Route::put('/', [UserController::class, 'update'])->name('auth.update');
+        Route::put('/preferences', [UserController::class, 'savePreferences'])->name('auth.savePreferences');
+    });
+
+    Route::prefix('feed')->group(function () {
+        Route::get('/', [FeedController::class, 'index'])->name('feed.index');
+        Route::put('/preferences', [FeedController::class, 'savePreferences'])->name('feed.savePreferences');
+        Route::get('/article/{id}', [ArticleController::class, 'index'])->name('article.index')->where('id', '[0-9]+');
+    });
 });
