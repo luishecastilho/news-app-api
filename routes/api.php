d9c09f05cab3f506db92026abbd4ceed92af21a2
luishecastilho <manocastilho07@gmail.com>
2023-05-16 00:07:32 -0300

    fixed api route name error

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,12 @@ Route::middleware('auth:api')->group(function () {
 
     Route::prefix('user')->group(function () {
         Route::get('/', [UserController::class, 'index'])->name('user.index');
-        Route::put('/', [UserController::class, 'update'])->name('auth.update');
-        Route::put('/preferences', [UserController::class, 'savePreferences'])->name('auth.savePreferences');
+        Route::put('/', [UserController::class, 'update'])->name('user.update');
+        Route::put('/preferences', [UserController::class, 'savePreferences'])->name('user.savePreferences');
     });
 
     Route::prefix('feed')->group(function () {
         Route::get('/', [FeedController::class, 'index'])->name('feed.index');
-        Route::put('/preferences', [FeedController::class, 'savePreferences'])->name('feed.savePreferences');
         Route::get('/article/{id}', [ArticleController::class, 'index'])->name('article.index')->where('id', '[0-9]+');
     });
 });
